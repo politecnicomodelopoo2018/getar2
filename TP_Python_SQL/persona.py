b923e699b254a2f0509f1ba9717c0e33173e1923
yitar <facundo.get2000@gmail.com>
2018-08-08 15:22:24 -0300

    archivo persona

---
+++ TP_Python_SQL/persona.py
@@ -0,0 +1,16 @@
+from TP_Python_SQL.db import DB
+
+class Persona(object):
+    dni = None
+    nombre = None
+    apellido = None
+
+    def Insertar(self):
+        DB.run("insert into Persona(dni,nombre,apellido) Values(" + str(self.dni) + ", '" + self.nombre + "', '" + self.apellido + "');")
+
+    def Borrar(self):
+        DB.run("Delete from Persona where dni = (" + str(self.dni) + ");")
+
+    def Modificar(self):
+        DB.run("UPDATE Persona SET nombre = '%s', apellido = '%s' WHERE dni = %i;" % (self.nombre, self.apellido, self.dni))
+
