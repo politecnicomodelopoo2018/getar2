653873534ec0f2544171dbfc02858d022760363e
yitar <facundo.get2000@gmail.com>
2018-08-08 15:23:46 -0300

    archivo para probar cosas

---
+++ TP_Python_SQL/testeos.py
@@ -0,0 +1,16 @@
+from TP_Python_SQL.persona import Persona
+from Python_Con_SQL1.categoria import Categoria
+from TP_Python_SQL.producto import Producto
+from TP_Python_SQL.compra import Compra
+
+p = Persona()
+c = Categoria()
+pr = Producto()
+co = Compra()
+
+unaCAt = Categoria()
+unaCAt.nombre_Categoria = "NULL"
+unaCAt.nombre_Categoria ="Lacteos"
+unaCAt.Insertar()
+
+
