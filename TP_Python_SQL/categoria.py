3353240e8d8f41e81b9c0d205587cb651bfac56d
yitar <facundo.get2000@gmail.com>
2018-08-08 15:20:28 -0300

    TP_Python_SQL

---
+++ TP_Python_SQL/categoria.py
@@ -0,0 +1,14 @@
+from db import DB
+
+class Categoria(object):
+    id_Categoria = None
+    nombre_Categoria = None
+
+    def Insertar(self):
+        DB.run("Insert into Categoria(id_Categoria,nombre_Categoria) Values("+ str(self.id_Categoria) +", '"+ self.nombre_Categoria +"');")
+
+    def Borrar(self):
+        DB.run("Delete from Categoria where id_Categoria = ("+ str(self.id_Categoria) +")")
+
+    def Modificar(self):
+        DB.run("UPDATE Persona SET nombre_Categoria = '%s' WHERE id_Categoria = %i;" % (self.nombre_Categoria, self.id_Categoria))
