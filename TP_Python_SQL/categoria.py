df36644de147c4f6bab52bb47d6f6643bafee8ed
yitar <facundo.get2000@gmail.com>
2018-08-10 10:32:48 -0300

    clase categoria

--- TP_Python_SQL/categoria.py
+++ TP_Python_SQL/categoria.py
@@ -8,9 +8,10 @@ class Categoria(object):
         DB.run("Insert into Categoria(id_Categoria,nombre_Categoria) Values("+ str(self.id_Categoria) +", '"+ self.nombre_Categoria +"');")
 
     def Borrar(self):
+        DB.run("Delete from Producto where Categoria_idtable1 = ("+ str(self.id_Categoria) + ")")
         DB.run("Delete from Categoria where id_Categoria = ("+ str(self.id_Categoria) +")")
 
     def Modificar(self):
-        DB.run("UPDATE Persona SET nombre_Categoria = '%s' WHERE id_Categoria = %i;" % (self.nombre_Categoria, self.id_Categoria))
+        DB.run("UPDATE Categoria SET nombre_Categoria = '%s' WHERE id_Categoria = %i;" % (self.nombre_Categoria, self.id_Categoria))
 
 
