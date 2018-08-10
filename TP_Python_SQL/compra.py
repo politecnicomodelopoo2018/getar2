ecf609e1d0e60c50876e00106aff095189772bfd
yitar <facundo.get2000@gmail.com>
2018-08-10 10:33:08 -0300

    clase compra

--- TP_Python_SQL/compra.py
+++ TP_Python_SQL/compra.py
@@ -9,6 +9,5 @@ class Compra(object):
     def Borrar(self,dni):
         DB.run("Delete from Compra where Persona_dni = (" + str(self.dni) + ");")
 
-    def Modificar(self):
-        DB.run("UPDATE Compra SET Persona_dni = %i, Producto_id_Producto = %i WHERE Persona_dni = %i;" % (self.dni, self.id, self.dni))
+
 
